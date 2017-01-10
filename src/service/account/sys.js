d897587b6a092090f73869979caa28b12fd9b46d
wangchao3012 <wangchao3012@163.com>
2017-01-11 00:22:04 +0800

    ---

--- src/service/account/sys.js
+++ src/service/account/sys.js
@@ -2,10 +2,13 @@
 const Tool = require('../../common/tool');
 const Cache = require('../../common/cache');
 const uuid = require('uuid/v4');
-const tasl = require('../../common/task')
-var sysService = {
-    sendSMS: function (d) {
+const tasl = require('../../common/task');
+const rp = require('request-promise');
 
+var sysService = {
+    sendSMS: async function (d) {
+        let result = await rp().then(res => res);
+        console.log('result:', result);
     }
 }
 module.exports = sysService;
